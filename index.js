77d977dabf018f359e544fdb8a5ca1e2507f1483
Ivan5 <converse_ivan@hotmail.com>
2017-05-13 21:10:57 -0500

    Update path

--- index.js
+++ index.js
@@ -7,9 +7,31 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('/hola/:name', (req,res)=>{
-  res.send({message: `Hola ${req.params.name}`})
+//Muestra todos los productos
+app.get('/api/product',(req,res) => {
+	res.send(200, {products: []})
 })
+//Acceder a un unico producto
+app.get('/api/product/:productId', (req,res) => {
+
+})
+
+//Subir productos
+app.post('/api/product',(req,res) => {
+	console.log(req.body)
+	res.status(200).send({message: 'El producto se ha recibido'})
+})
+
+//Actulizar productos
+app.put('/api/product/:productId', (req,res) => {
+
+})
+
+//Borrar productos
+app.delete('/api/product/:productId',(req,res) => {
+
+})
+
 app.listen(port, () => {
   console.log(`Api rest corriendo en el puerto ${port}`)
 })
