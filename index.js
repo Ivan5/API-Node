a5bdefe5de6aa01a79034a8343af7ea700e6e1f4
Ivan5 <converse_ivan@hotmail.com>
2017-05-13 21:58:33 -0500

    Update get url

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ app.use(bodyParser.json())
 
 //Muestra todos los productos
 app.get('/api/product',(req,res) => {
-	res.send(200, {products: []})
+	Product.find({},(err,products) => {
+		if (err) return res.status(500).send({message: `Error al realizar la peticion ${err}`})
+		if (!products) return res.status(404).send({message : `No existen productos`})
+
+		res.send(200, {products})
+
+	})
 })
 //Acceder a un unico producto
 app.get('/api/product/:productId', (req,res) => {
+	let productId = req.params.productId
+	Product.findById(productId, (err,product) => {
+		if (err) return res.status(500).send({message: `Error al realizar la peticion ${err}`})
+		if (!product) return res.status(404).send({message : `El producto no existe`})
+
+
+		res.status(200).send({product})
+	})
 
 })
 
